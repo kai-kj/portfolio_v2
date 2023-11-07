df712c4a3d34c284814ad9502fba7ee6bf7c059b
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2023-11-07 20:47:21 +0100

    nicer photo page

--- site_source/pages/photos.py
+++ site_source/pages/photos.py
@@ -1,20 +1,37 @@
 import pathlib
+import pycountry
+
+
+def unicode_flag(country_code):
+    off = ord('🇦') - ord('A')
+    return chr(ord(country_code[0]) + off) + chr(ord(country_code[1]) + off)
+
 
 src_dir = pathlib.Path(globals()["src_dir"])
 
 src_files = list((src_dir / "assets").glob("*"))
-src_files = [f for f in src_files if "photos_" in f.name]
+src_files = [f for f in src_files if "photo_" in f.name]
 src_files.sort(reverse=True)
 
 output = "Title: Photos\n"
+output += "raw: true\n"
 output += "Description: A collection of photos I've taken in various countries.\n"
 output += "Icon: logo.jpg\n"
 output += "Pos: 02_00\n\n"
-output += "# Photos\n\n"
-output += "A collection of photos I've taken in various countries.\n\n"
+output += "<h1>Photos</h1>\n\n"
+output += "<p>A collection of photos I've taken in various countries.</p>\n\n"
 
 for src_file in src_files:
-    title = "/".join(src_file.name.split("_")[1].split("-")[:3])
+    photo_info = src_file.name.split(".")[0]
+    date = "/".join(photo_info.split("_")[1].split("-")[:3])
+    country_code = photo_info.split("_")[2].upper()
 
-    output += f"## {title}\n\n"
-    output += f"![image from {title}](assets/{src_file.name})\n\n"
+    country_name = pycountry.countries.get(alpha_2=country_code).name
+
+    output += f"""
+        <div class="thumbnail">
+            <img alt="image from {date}" src="assets/{src_file.name}"><a href="expanded_images/{src_file.name}.html">click to expand</a>
+        </div>\n
+    """
+
+    output += f"{unicode_flag(country_code)} {country_name}, {date}\n<br><br>\n"
