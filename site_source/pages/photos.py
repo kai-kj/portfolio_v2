9d1f85f396f0fee2779057479f23d8aa5c7579a5
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2024-01-08 20:05:32 +0100

    better image handling

--- site_source/pages/photos.py
+++ site_source/pages/photos.py
@@ -1,5 +1,4 @@
 import pathlib
-import pycountry
 
 
 def unicode_flag(country_code):
@@ -14,24 +13,16 @@ src_files = [f for f in src_files if "photo_" in f.name]
 src_files.sort(reverse=True)
 
 output = "Title: Photos\n"
-output += "raw: true\n"
 output += "Description: A collection of photos I've taken in various countries.\n"
 output += "Icon: logo.jpg\n"
 output += "Pos: 02_00\n\n"
-output += "<h1>Photos</h1>\n\n"
-output += "<p>A collection of photos I've taken in various countries.</p>\n\n"
+output += "# Photos\n\n"
+output += "A collection of photos I've taken in various countries.\n\n"
 
 for src_file in src_files:
     photo_info = src_file.name.split(".")[0]
     date = "/".join(photo_info.split("_")[1].split("-")[:3])
     country_code = photo_info.split("_")[2].upper()
 
-    country_name = pycountry.countries.get(alpha_2=country_code).name
-
-    output += f"""
-        <div class="thumbnail">
-            <img alt="image from {date}" src="thumbnails/{src_file.name}"><a href="expanded_images/{src_file.name}.html">click to expand</a>
-        </div>\n
-    """
-
-    output += f"<div align=\"right\">{unicode_flag(country_code)} {country_name}, {date}</div>\n<br><br>\n"
+    output += f"<br><div align=\"center\">{unicode_flag(country_code)}&ensp;{date}</div>\n"
+    output += f"![photo from {date}](assets/{src_file.name})\n"
